c07174afb9927e10b2d375881c16b3a59e0b7543
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-24 23:25:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-
-function forEach(iterable, callback){
-  if(Array.isArray(iterable)){
-    for(let i =0, l = iterable.length; i < l; i++){
-      callback(iterable[i], i , iterable)
+function forEach(iterable, callback) {
+  if (Array.isArray(iterable)) {
+    for (let i = 0, l = iterable.length; i < l; i++) {
+      const element = iterable[i];
+      callback(element, i, iterable);
     }
-  }
-  else if (typeof iterable === 'object'){
-    for(const key in iterable{
-      callback(iterable[key],key,iterable)
-    })
+  } else if (typeof iterable === 'object') {
+    for (const key in iterable) {
+      const value = iterable[key];
+      callback(value, key, iterable);
+    }
   }
 }
