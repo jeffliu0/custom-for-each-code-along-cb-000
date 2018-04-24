079185265f77d93397fac3b68a57418f53392815
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-24 23:27:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function forEach(iterable, callback) {
     }
   }
   else if (typeof iterable === 'object') {
-    for(const key in iterable{
+    for(const key in iterable){
       callback(iterable[key],key,iterable)
     })
   }
